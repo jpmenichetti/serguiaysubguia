4f86b9ae9e1b6974cfbe72f2fc4f28566b7c7756
Juan Pablo Menichetti <jpmenichetti@sisdef.cl>
2017-11-25 20:23:30 -0300

    transiciones
    Signed-off-by: Juan Pablo Menichetti <jpmenichetti@sisdef.cl>

--- www/js/gestures.js
+++ www/js/gestures.js
@@ -1,9 +1,20 @@
+
 function configureGestures(contentId, previousPage, nextPage){
 		var myElement = document.getElementById(contentId);
 		
-		var mc = new Hammer(myElement);
-		mc.get('pinch').set({ enable: true });
-		mc.get('swipe').set({ direction : Hammer.DIRECTION_ALL });
+		var mc = new Hammer(myElement
+			,{
+				preventDefault: false
+			//touchAction: auto
+			//recognizers: [
+			//	[Hammer.Pan,{direction: Hammer.DIRECTION_HORIZONTAL}]
+		}
+		);
+		mc.get('rotate').set({ enable: false });
+		
+		mc.get('swipe').set({ direction : Hammer.DIRECTION_HORIZONTAL });
+		mc.get('pinch').set({ enable: false });
+		//mc.get('pinch').set({ enable: true });
 		mc.on("pinchin", function(ev) {
 			var currentFS = myElement.style.fontSize;
 			var value=100;
@@ -30,14 +41,35 @@ function configureGestures(contentId, previousPage, nextPage){
 			}
 			myElement.style.fontSize=value+"%";
 		});
-		myElement.style.touchAction="pan-y";
-		
+		//myElement.style.touchAction="pan-y";
+		//myElement.style.touchAction="auto";
 
-		mc.on("swiperight", function(ev) {
+		/*mc.on("swiperight", function(ev) {
 			window.location.replace(previousPage);
 		});
 		mc.on("swipeleft", function(ev) {
 			window.location.replace(nextPage);
 		});
+		mc.on("swipeup",function(ev) {
+			window.scrollBy(0,100);
+		});
+		mc.on("swipedown",function(ev) {
+			window.scrollBy(0,-100);
+		});*/
+		mc.on("swipe", function(ev){
+			//alert(ev.direction);
+			event.preventDefault();
+			if(ev.direction == Hammer.DIRECTION_LEFT)
+			{
+				window.location.replace(nextPage);
+			}
+			else if(ev.direction == Hammer.DIRECTION_RIGHT)
+			{
+				window.location.replace(previousPage);
+			}
+			else
+			{
+				alert(ev.direction);
+			}
+		});
 	}
-	
